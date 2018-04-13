be028bbbb4fbf445d9900264999f5c0f4c3a5523
Hulisani Mudimeli <hulisanimudimeli@gmail.com>
2018-04-13 09:55:27 +0200

    Travis Test Execution

--- testing/Test4.java
+++ testing/Test4.java
@@ -8,5 +8,6 @@ public class Test4{
 		}catch(InterruptedException e){
 			System.out.println("...");
 		}
-		System.out.println("==> Dummy Project Deleted...");			}
+		System.out.println("==> Dummy Project Deleted...");			
+	}
 }
